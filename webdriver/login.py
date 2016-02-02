5153902fe9a3338bcfef84c6c5337a57a9f78374
vicgr <vicgr563@student.liu.se>
2016-02-02 14:48:55 +0100

    some fixes

--- webdriver/login.py
+++ webdriver/login.py
@@ -5,14 +5,27 @@ import sys
 import test_reporter
 
 
+check_loginfail= False
+check_login = False
+
 def test():
+
     driver = webdriver.Firefox()
     driver.get("https://t1.storedsafe.com/")
     page = loginfailtest(driver,page_login.PageLogin(driver), "a", "b")
+    if not check_loginfail:
+        end_Tests()
+        exit(1)
     u= str(input('username: '))
     p= str(input('password+yubikey: '))
     page = logintest(driver,page,u,p)
-    reporter.printreport()
+    if not check_login:
+        end_Tests()
+        exit(1)
+
+    print(check_loginfail +""+check_login)
+    end_Tests()
+
 
 def loginfailtest(driver, page, username, password):
     page = page_login.PageLogin.login_incorrectly(page, username, password)
@@ -20,10 +33,10 @@ def loginfailtest(driver, page, username, password):
         assert str(driver.current_url) == 'https://t1.storedsafe.com/'
     except AssertionError:
         reporter.add_failure(1,"loginfailuretest","is at: "+driver.current_url,"expected to be at: https://t1.storedsafe.com/")
-        #print ('Test failed: expected to reach login page. Is at '+driver.current_url)
         return page
-    #print("test successful: incorrect login failed")
     reporter.add_success(1,"loginfailuretest","did not login")
+    global check_loginfail
+    check_loginfail= True
     return page
 
 
@@ -33,19 +46,18 @@ def logintest(driver, page, username,password):
         assert str(driver.current_url).startswith('https://t1.storedsafe.com/groups.php')
     except AssertionError:
         reporter.add_failure(2,"logintest","is at: "+driver.current_url,"expected to be at: https://t1.storedsafe.com/groups.php")
-        #print('Test failed: expected to reach vaultpage. Is at '+driver.current_url)
-        #print ('Could not login, exiting')
         return page
 
     reporter.add_success(2,"logintest","correct login successful - logged in")
-
-    #print("test successful: logged in")
+    global check_login
     check_login = True
 
     return page
 
+def end_Tests():
+    reporter.printreport()
+
 reporter = test_reporter.testreporter()
-check_login = False
 
 test()
 
