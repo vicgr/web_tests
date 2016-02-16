f427f1faffd2ec1225dde978ca10ef4db45b6fa7
Victor <vicgr563@student.liu.se>
2016-02-16 17:54:08 +0100

    capybara beginning

---
+++ capybara/user_logs_in.rb
@@ -0,0 +1,18 @@
+#require 'spec_helper'
+require 'capybara'
+
+Capybara.app_host = "https://t1.storedsafe.com/"
+Capybara.run_server = false
+#Capybara.default_driver = :selenium
+
+session = Capybara::Session.new :selenium
+session.visit("")
+
+
+if session.has_title? session.title do
+  puts "yas"
+end
+puts "no"
+
+
+end
